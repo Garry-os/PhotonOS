af3dca89e6e9a31f3053eda2fb6c0df150a753ed
Garry-os <baong2408@gmail.com>
2026-01-13 18:32:07 +0700

    Used GNU EFI headers

--- osloader/src/main.c
+++ osloader/src/main.c
@@ -1,7 +1,11 @@
 #include <efi.h>
 
-EFI_STATUS EFIAPI efi_main(void)
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\r\n");
+
+	while (1);
+
 	return EFI_SUCCESS; // Completed successfully
 }
 
