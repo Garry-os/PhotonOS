73840cfa2ff8892d3d31a1866e900ad05db2f5d8
Garry-os <baong2408@gmail.com>
2026-01-13 18:46:54 +0700

    Implemented a key pressing test

--- osloader/src/main.c
+++ osloader/src/main.c
@@ -2,7 +2,15 @@
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\r\n");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Press any key...\r\n");
+
+	EFI_INPUT_KEY key;
+	while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS)
+	{
+
+	}
+
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Key Pressed\r\n");
 
 	while (1);
 
