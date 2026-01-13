37c8bd7468eccab14415cffead3bf12460646947
Garry-os <baong2408@gmail.com>
2026-01-13 18:52:24 +0700

    Implemented a function for printing

--- osloader/src/main.c
+++ osloader/src/main.c
@@ -1,8 +1,25 @@
 #include <efi.h>
 
+EFI_HANDLE g_ImageHandle;
+EFI_SYSTEM_TABLE* g_SystemTable;
+
+void SetupGlobalVars(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+{
+	g_ImageHandle = ImageHandle;
+	g_SystemTable = SystemTable;
+}
+
+void print(CHAR16* str)
+{
+	g_SystemTable->ConOut->OutputString(g_SystemTable->ConOut, str);
+}
+
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Press any key...\r\n");
+	// Setup global vars
+	SetupGlobalVars(ImageHandle, SystemTable);
+
+	print(L"Press any key to boot...\r\n");
 
 	EFI_INPUT_KEY key;
 	while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key) != EFI_SUCCESS)
@@ -10,7 +27,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable
 
 	}
 
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Key Pressed\r\n");
+	print(L"Booting...\r\n");
 
 	while (1);
 
