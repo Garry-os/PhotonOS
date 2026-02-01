acd53ef16209e10c22ccc77d776b8a344da9e476
Garry-os <baong2408@gmail.com>
2026-02-01 09:23:10 +0700

    Complete IRQ handling system

--- kernel/src/arch/x86_64/irq.c
+++ kernel/src/arch/x86_64/irq.c
@@ -2,6 +2,7 @@
 #include <x86_64/pic.h>
 #include <x86_64/isr.h>
 #include <qemu/print.h>
+#include <stddef.h>
 
 #define PIC_REMAP_OFFSET 0x20
 
@@ -10,7 +11,16 @@ IRQHandler g_IRQHandlers[16];
 void IRQ_Handler(cpu_registers_t* context)
 {
 	int irq = context->interrupt_number - PIC_REMAP_OFFSET;
-	dbg_printf("IRQ: %d\n", irq);
+
+	if (g_IRQHandlers[irq] != NULL)
+	{
+		g_IRQHandlers[irq](context);
+	}
+	else
+	{
+		dbg_printf("Unhandled IRQ: %d\n", irq);
+	}
+
 	PIC_SendEOI(context->interrupt_number);
 }
 
